538ccce6c315b4ce44739e1bc16f07480f26a5f6
Johniel <johniel.s.m@gmail.com>
2013-08-15 13:59:41 +0900

    Update speed.cljs

--- src/cljs/speed.cljs
+++ src/cljs/speed.cljs
@@ -1,6 +1,6 @@
 (ns cube.speed
-  (:use [cljs.core.async :only [chan <! put! !>]])
-  (:use-macros [cljs.core.async.macros :only [go]]))
+  (:use [cljs.core.async :only [chan <! >! put! alts! alts!! timeout]])
+  (:use-macros [cljs.core.async.macros :only [go alt!]]))
 
 (def speed-x (atom 2.0))
 (def speed-y (atom 0.9))
@@ -15,15 +15,15 @@
 
 (def input-chan
   (let [c (chan)]
-    (.addEventListener js/document "keydown" #(put! c (.-keyCode %)))
+    (.addEventListener js/document "keydown" #(>! c (.-keyCode %)))
     c))
 
-(defn ^:export main []
-  (go (loop []
-        (let [key-code (<! input-chan)]
+(defn ^:export change-speed []
+  (go (let [[key-code ch] (alts! [input-chan (timeout 1)])]
+        (if (= ch input-chan)
           (case key-code
-                up-key    (reset! speed-y (+ 0.5 speed-y))
-                down-key  (reset! speed-y (- 0.5 speed-y))
-                right-key (reset! speed-x (+ 0.5 speed-x))
-                left-key  (reset! speed-x (- 0.5 speed-x)))
-       (recur)))))
+                38 (reset! speed-y (+ 0.1 @speed-y))
+                40 (reset! speed-y (- 0.1 @speed-y))
+                39 (reset! speed-x (+ 0.1 @speed-x))
+                37 (reset! speed-x (- 0.1 @speed-x))
+                nil)))))
